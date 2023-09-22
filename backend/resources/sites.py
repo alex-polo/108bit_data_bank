f633f8bb74a97a973e98ef1808b4fcd48f6a46c5
Medved <boorelom87@gmail.com>
2023-09-22 15:13:40 +0300

    First

--- backend/resources/sites.py
+++ backend/resources/sites.py
@@ -10,15 +10,81 @@ sites_list: list = [
         task='server.tasks.v1.bolid.parser.task',
         is_enabled=True
     ),
+    Resource(
+        name='macroscop',
+        system_name='macroscop',
+        url='https://macroscop.com',
+        tag='#Macroscop',
+        field_tags=['#Системы_автоматики', '#Системы_безопасности'],
+        task='server.tasks.v1.macroscop.parser.task',
+        is_enabled=True
+    ),
+    Resource(
+        name='rubezh',
+        system_name='rubezh',
+        url='https://rubezh.ru',
+        tag='#Rubezh',
+        field_tags=['#Системы_автоматики', '#Системы_безопасности'],
+        task='server.tasks.v1.rubezh.parser.task',
+        is_enabled=True
+    ),
+    Resource(
+        name='parsec',
+        system_name='parsec',
+        url='https://parsec.ru',
+        tag='#Parsec',
+        field_tags=['#Системы_автоматики', '#Системы_безопасности'],
+        task='server.tasks.v1.parsec.parser.task',
+        is_enabled=True
+    ),
+    Resource(
+        name='perco',
+        system_name='perco',
+        url='https://perco.ru',
+        tag='#Perco',
+        field_tags=['#Системы_автоматики', '#Системы_безопасности'],
+        task='server.tasks.v1.perco.parser.task',
+        is_enabled=True
+    ),
+    Resource(
+        name='sigur',
+        system_name='sigur',
+        url='https://sigur.com',
+        tag='#Sigur',
+        field_tags=['#Системы_автоматики', '#Системы_безопасности'],
+        task='server.tasks.v1.sigur.parser.task',
+        is_enabled=True
+    ),
+    Resource(
+        name='ironlogic',
+        system_name='ironlogic',
+        url='https://ironlogic.ru',
+        tag='#Ironlogic',
+        field_tags=['#Системы_автоматики', '#Системы_безопасности'],
+        task='server.tasks.v1.ironlogic.parser.task',
+        is_enabled=True
+    ),
+Resource(
+        name='bastion',
+        system_name='bastion',
+        url='https://bastion-tech.ru',
+        tag='#Bastion',
+        field_tags=['#Системы_автоматики', '#Системы_безопасности'],
+        task='server.tasks.v1.bastion.parser.task',
+        is_enabled=True
+    ),
+Resource(
+        name='argus',
+        system_name='argus',
+        url='https://argus-spectr.ru',
+        tag='#Argus',
+        field_tags=['#Системы_автоматики', '#Системы_безопасности'],
+        task='server.tasks.v1.argus.parser.task',
+        is_enabled=True
+    ),
 ]
 
-# https://macroscop.com/produkty
+
 # Eltys
-# https://bolid.ru/production/
-# https://rubezh.ru/brands
-# https://www.parsec.ru/  !!!!!
-# https://www.perco.ru/products/
-# https://sigur.com/
-# https://ironlogic.ru/il_new.nsf/htm/ru_home
-# https://bastion-tech.ru/
-# https://argus-spectr.ru/
+
+
