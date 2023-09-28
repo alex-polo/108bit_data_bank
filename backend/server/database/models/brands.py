98446a6b7bd94f8da6f3c2ceabeeb88ab8728f0a
Medved <boorelom87@gmail.com>
2023-09-28 13:08:22 +0300

    Anotherr

--- backend/server/database/models/brands.py
+++ backend/server/database/models/brands.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from xmlrpc.client import Boolean
 
-from sqlalchemy import Table, Column, ForeignKey, Integer
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Text, DateTime
 
 from database.lib import Base
 
@@ -10,14 +11,15 @@ brands = Table(
     "brands",
     Base.metadata,
     Column("id", Integer(), primary_key=True),
-    Column("brand", Integer(), primary_key=True),
-    Column("created_date", Integer(), primary_key=False,default=func.now),
+    Column("brand", String(500), nullable=False),
     Column("field_tag", Integer(), primary_key=True),
     Column("firmware", Integer(), primary_key=True),
     Column("hardware", Integer(), primary_key=True),
-    Column("info", Integer(), primary_key=True),
+    Column("info", Text(), nullable=False),
     Column("support", Integer(), primary_key=True),
     Column("is_enable", Boolean(), primary_key=True),
+    Column("created_on", DateTime(), default=datetime.now),
+    Column("updated_on", DateTime(), default=datetime.now, onupdate=datetime.now),
     #ИМЯ !?
     # Column("right_id", ForeignKey("right_table.id"), primary_key=True),
 )
