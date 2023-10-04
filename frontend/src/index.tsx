7e428b96017987b0b2b5a6b7e567d43fd12ffc26
Alex <polonnikov.alexander@gmail.com>
2023-10-04 17:06:15 +0300

    commit

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,10 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import ErrorPage from './ErrorPage';
+import PageInfo from './components/pages/info/Info';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import PageInfo from './components/pages/info/Info';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
